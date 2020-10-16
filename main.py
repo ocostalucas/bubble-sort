3f4dedd80f6ffa9647a64b8c4ae1309c9c137583
ocostalucas <olucascosta98@gmail.com>
2020-10-16 11:01:44 -0300

    Add read file function

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ def write_file(value):
         f.write("%s\n" % item)
     f.close()
 
+def read_file(value):
+    f = open('file_'+ str(value) +'.txt', "r")
+    data = f.read().split('\n')[:-1]
+    f.close()
+    return data
+
 write_file(10)
+print(read_file(10))
