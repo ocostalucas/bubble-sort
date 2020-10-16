6421e3ef88ef8e70d1891f7e9176ec645b6ac45e
ocostalucas <olucascosta98@gmail.com>
2020-10-16 11:19:04 -0300

    Add exchange counter and comparisons

--- bubble_sort_v1.py
+++ bubble_sort_v1.py
@@ -1,10 +1,19 @@
 # Bubble Sort version 1
+import main
 
-def bubble_sort(lista):
-    for i in range(len(lista)):
-        for j in range(len(lista) - 1):
-            if lista[j] > lista[j+1]:
-                aux = lista[j]
-                lista[j] = lista[j+1]
-                lista[j+1] = aux
-    return lista
+list = main.read_file(10)
+comparisons = 0
+exchanges = 0
+
+
+for i in range(len(list)):
+    for j in range(len(list) - 1):
+        comparisons += 1
+        if list[j] > list[j+1]:
+            aux = list[j]
+            list[j] = list[j+1]
+            list[j+1] = aux
+            exchanges += 1
+
+print('Quantidade de quantidade de comparações: ', str(comparisons))
+print('Quantidade de trocas: ', str(exchanges))
